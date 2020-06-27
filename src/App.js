a30a7aab6aa68db507dd54138deca58328839a8f
Lwin Ye <lwin_ye@yahoo.com>
2020-06-27 07:31:35 -0700

    setup initial routes with navbar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Navbar from './components/navbar.component';
+import CreateExercise from './components/create-exercise.component';
+import CreateUser from './components/create-user.component';
+import EditExercise from './components/edit-exercises.component';
+import ExercisesList from './components/exercises-list.component';
+
 function App() {
-  return <div className='container'>This is test!</div>;
+  return (
+    <Router>
+      <Navbar />
+      <br />
+      <Route path='/' exact component={ExercisesList} />
+      <Route path='/edit/:id' exact component={EditExercise} />
+      <Route path='/create' exact component={CreateExercise} />
+      <Route path='/user' exact component={CreateUser} />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateUser extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is create user component</p>
+      </div>
+    );
+  }
+}
