a41a2f92ff24a1595d26ef308285a4c709743ce5
Lwin Ye <lwin_ye@yahoo.com>
2020-06-27 01:08:43 -0700

    fixed typo in exercise delete route

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -30,7 +30,7 @@ router.route('/:id').get((req, res) => {
 
 router.route('/:id').delete((req, res) => {
   Exercise.findByIdAndDelete(req.params.id)
-    .then(() => req.json('Deleted!'))
+    .then(() => res.json('Deleted!'))
     .catch((err) => res.status(400).json('Error: ' + err));
 });
 
