d1100e079875e5f67eece606419f89783bafc947
Lwin Ye <lwin_ye@yahoo.com>
2020-06-27 01:02:43 -0700

    more exercise routes

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -22,4 +22,31 @@ router.route('/add').post((req, res) => {
     .catch((err) => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => res.json(exercise))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => req.json('Deleted!'))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Exercise.findById(req.params.id).then((exercise) => {
+    const { username, description, duration, date } = req.body;
+    exercise.username = username;
+    exercise.description = description;
+    exercise.duration = Number(duration);
+    exercise.date = Date.parse(date);
+
+    exercise
+      .save()
+      .then(() => res.json('exercise updated!'))
+      .catch((err) => res.status(400).json('Error: ' + err));
+  });
+});
+
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,7 +9,6 @@ router.route('/').get((req, res) => {
 
 router.route('/add').post((req, res) => {
   const username = req.body.username;
-  console.log(username);
   const newUser = new User({ username });
 
   newUser
